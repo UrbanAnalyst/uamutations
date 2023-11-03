e7551aca6d16c913bf5f1024081aaa267df3564e
mpadge <mark.padgham@email.com>
2023-11-03 17:23:21 +0100

    sort values of both vectors

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ const FNAME2: &str = "dat2.json";
 const VARNAME: &str = "transport";
 
 fn main() {
-    let values1 = readfile::readfile(FNAME1, VARNAME, NENTRIES);
-    let values2 = readfile::readfile(FNAME2, VARNAME, NENTRIES);
+    let mut values1 = readfile::readfile(FNAME1, VARNAME, NENTRIES);
+    let mut values2 = readfile::readfile(FNAME2, VARNAME, NENTRIES);
+    values1.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    values2.sort_by(|a, b| a.partial_cmp(b).unwrap());
 
     for ((index1, number1), (_, number2)) in values1.iter().enumerate().zip(values2.iter().enumerate()) {
         println!("Index: {}: ( {}, {} )", index1 + 1, number1, number2);
