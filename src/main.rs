f28ab699042a986d1566678a3f48b0eefe814946
mpadge <mark.padgham@email.com>
2023-11-03 16:10:42 +0100

    read two files

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 mod readfile;
 
 const NENTRIES: usize = 100;
-const FILENAME: &str = "dat1.json";
+const FNAME1: &str = "dat1.json";
+const FNAME2: &str = "dat2.json";
 const VARNAME: &str = "transport";
 
 fn main() {
-    let values = readfile::readfile(FILENAME, VARNAME, NENTRIES);
+    let values1 = readfile::readfile(FNAME1, VARNAME, NENTRIES);
+    let values2 = readfile::readfile(FNAME2, VARNAME, NENTRIES);
 
-    for (index, number) in values.iter().enumerate() {
-        println!("{}: {}", index + 1, number);
+    for ((index1, number1), (_, number2)) in values1.iter().enumerate().zip(values2.iter().enumerate()) {
+        println!("Index: {}: ( {}, {} )", index1 + 1, number1, number2);
     }
 }
