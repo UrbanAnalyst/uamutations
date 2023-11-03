a2639fd514a33bd70b612aec6927ac187f46181f
mpadge <mark.padgham@email.com>
2023-11-03 15:40:32 +0100

    read one geojson file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::File;
+use std::io::BufReader;
+use geojson::GeoJson;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("dat1.json").unwrap();
+    let reader = BufReader::new(file);
+
+    let geojson = GeoJson::from_reader(reader).unwrap();
+
+    let mut values = Vec::new();
+
+    if let GeoJson::FeatureCollection(collection) = geojson {
+        for feature in collection.features {
+            if let Some(properties) = feature.properties {
+                if let Some(value) = properties.get("transport") {
+                    if let Some(number) = value.as_f64() {
+                        values.push(number);
+                    }
+                }
+            }
+        }
+    }
+
+    for number in &values {
+        println!("{}", number);
+    }
 }
