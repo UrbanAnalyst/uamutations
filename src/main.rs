2f507570b472dd053ff601bcb18012e85b2f3f83
mpadge <mark.padgham@email.com>
2023-11-03 15:51:21 +0100

    Only extract fixed number of entries

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use std::fs::File;
 use std::io::BufReader;
 use geojson::GeoJson;
 
+const NENTRIES: usize = 100;
+const FILENAME: &str = "dat1.json";
+const VARNAME: &str = "transport";
+
 fn main() {
 
-    let filename = "dat1.json";
-    let varname = "transport";
-
-    let file = File::open(filename).unwrap();
+    let file = File::open(FILENAME).unwrap();
     let reader = BufReader::new(file);
 
     let geojson = GeoJson::from_reader(reader).unwrap();
@@ -16,8 +17,11 @@ fn main() {
 
     if let GeoJson::FeatureCollection(collection) = geojson {
         for feature in collection.features {
+            if values.len() >= NENTRIES {
+                break;
+            }
             if let Some(properties) = feature.properties {
-                if let Some(value) = properties.get(varname) {
+                if let Some(value) = properties.get(VARNAME) {
                     if let Some(number) = value.as_f64() {
                         values.push(number);
                     }
