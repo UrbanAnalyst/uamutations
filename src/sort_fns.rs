635f15433d3a596b7c445fe19c7b4dc10c803543
mpadge <mark.padgham@email.com>
2023-11-07 11:07:15 +0100

    add unit tests for 'get_ordered_index' fn

--- src/sort_fns.rs
+++ src/sort_fns.rs
@@ -10,3 +10,22 @@ pub fn get_ordering_index(vals: &Vec<f64>, is_abs: bool) -> Vec<usize> {
     let index: Vec<_> = pairs.iter().map(|&(index, _)| index).collect();
     index
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_ordering_index() {
+        let vals = vec![1.0, -2.0, 3.0, -4.0, 5.0];
+        let expected = vec![4, 2, 0, 1, 3]; // Indices of vals sorted in descending order
+        assert_eq!(get_ordering_index(&vals, false), expected);
+    }
+
+    #[test]
+    fn test_get_ordering_index_abs() {
+        let vals = vec![1.0, -2.0, 3.0, -4.0, 5.0];
+        let expected = vec![4, 3, 2, 1, 0]; // Indices of absolute values of vals sorted in descending order
+        assert_eq!(get_ordering_index(&vals, true), expected);
+    }
+}
