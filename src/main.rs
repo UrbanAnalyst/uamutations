09b7cd0ec6c98a487f1db945bad340909b6ae541
mpadge <mark.padgham@email.com>
2023-11-16 13:29:12 +0100

    rm inferior pca and kd-tree methods for #1

--- src/main.rs
+++ src/main.rs
@@ -2,60 +2,34 @@
 //! Analyst](https://urbananalyst.city). The algorithm mutates selected properties for one city to
 //! become more like those of another selected city.
 
-use std::fs::File;
-use std::io::Write;
-
 extern crate uamutations;
 pub mod read_write_file;
 pub mod vector_order;
 
-const NENTRIES: usize = 2000;
+const NENTRIES: usize = 1000;
 
-// const FNAME1: &str = "berlin.json";
-// const FNAME2: &str = "paris.json";
-const FNAME1: &str = "./test_resources/dat1.json";
-const FNAME2: &str = "./test_resources/dat2.json";
+const FNAME1: &str = "berlin.json";
+const FNAME2: &str = "paris.json";
+// const FNAME1: &str = "./test_resources/dat1.json";
+// const FNAME2: &str = "./test_resources/dat2.json";
 const VARNAME: &str = "bike_index";
 const OUTFILENAME: &str = "output.txt";
-
-fn profile_methods(nentries: usize) -> (String, String, String) {
-    let varsall: Vec<String> = vec![VARNAME.to_string()];
-    let varextra = vec!["natural".to_string()];
-    let varsall = [varsall, varextra].concat();
-    let (_index1, values1, _groups1) = read_write_file::readfile(FNAME1, &varsall, nentries);
-    let (_index2, values2, _groups2) = read_write_file::readfile(FNAME2, &varsall, nentries);
-
-    use std::time::Instant;
-
-    let start = Instant::now();
-    let _order_kd2 = vector_order::order_vectors_kd(&values1, &values2);
-    let duration_kd = format!("{:.4}", start.elapsed().as_secs_f64());
-
-    let start = Instant::now();
-    let _order2 = vector_order::order_vectors(&values1, &values2);
-    let duration_dists = format!("{:.4}", start.elapsed().as_secs_f64());
-
-    let start = Instant::now();
-    let _pca1 = vector_order::pca(&values1);
-    let duration_pca = format!("{:.4}", start.elapsed().as_secs_f64());
-
-    (duration_kd, duration_dists, duration_pca)
-}
 
 /// Entry point for the Urban Analyst mutation algorithm.
 ///
 /// This exists only to locally call and run the library.
 fn main() {
-    let mut file = File::create("timings.csv").expect("Unable to create file");
-    writeln!(file, "n, kd, dists, pca").expect("Unable to write to file");
-    for n in 1..20 {
-        // let nentries = n * 100;
-        let nentries = n * 10;
-        let times = profile_methods(nentries);
-        writeln!(file, "{}, {}, {}, {}", nentries, times.0, times.1, times.2)
-            .expect("Unable to write to file");
-        println!("n = {}", nentries);
-    }
+    let varsall: Vec<String> = vec![VARNAME.to_string()];
+    let varextra = vec!["natural".to_string()];
+    let varsall = [varsall, varextra].concat();
+    let (_index1, values1, _groups1) = read_write_file::readfile(FNAME1, &varsall, NENTRIES);
+    let (_index2, values2, _groups2) = read_write_file::readfile(FNAME2, &varsall, NENTRIES);
+
+    use std::time::Instant;
+    let start = Instant::now();
+    let _order2 = vector_order::order_vectors(&values1, &values2);
+    let duration_dists = format!("{:.4}", start.elapsed().as_secs_f64());
+    println!("time = {}", duration_dists);
 
     let varextra: Vec<String> = Vec::new();
     // let varextra = vec!["natural".to_string()];
--- src/vector_order.rs
+++ src/vector_order.rs
@@ -1,42 +1,3 @@
-pub fn order_vectors_kd(vector1: &[Vec<f64>], vector2: &[Vec<f64>]) -> Vec<usize> {
-    use kdtree::distance::squared_euclidean;
-    use kdtree::KdTree;
-    use std::collections::HashSet;
-
-    let mut kdtree = KdTree::new(vector1[0].len());
-    for (i, point) in vector2.iter().enumerate() {
-        kdtree.add(point, i).unwrap();
-    }
-
-    let mut used_indices = HashSet::new();
-    let mut mapping = Vec::new();
-
-    for i in 0..vector1[0].len() {
-        let point1 = vector1.iter().map(|v| v[i]).collect::<Vec<_>>();
-        let mut min_distance = f64::MAX;
-        let mut min_index = 0;
-
-        for j in 0..vector2[0].len() {
-            if used_indices.contains(&j) {
-                continue;
-            }
-
-            let point2 = vector2.iter().map(|v| v[j]).collect::<Vec<_>>();
-            let distance = squared_euclidean(&point1, &point2);
-
-            if distance < min_distance {
-                min_distance = distance;
-                min_index = j;
-            }
-        }
-
-        used_indices.insert(min_index);
-        mapping.push(min_index);
-    }
-
-    mapping
-}
-
 pub fn order_vectors(vector1: &[Vec<f64>], vector2: &[Vec<f64>]) -> Vec<usize> {
     use std::collections::HashSet;
 
@@ -74,38 +35,4 @@ fn squared_euclidean(point1: &[f64], point2: &[f64]) -> f64 {
         .zip(point2.iter())
         .map(|(&x1, &x2)| (x1 - x2).powi(2))
         .sum()
-}
-
-use ndarray::{Array2, Axis};
-use ndarray_linalg::{Eigh, UPLO};
-
-pub fn pca(vector1: &[Vec<f64>]) -> Array2<f64> {
-    let rows = vector1.len(); // multivariate dimensions
-    let cols = vector1[0].len(); // nobs
-    let flattened: Vec<f64> = vector1.iter().flatten().cloned().collect();
-    let data = Array2::from_shape_vec((rows, cols), flattened).unwrap();
-
-    // Calculate the mean of the data
-    let mean = data.mean_axis(Axis(0)).unwrap();
-
-    // Subtract the mean from the data
-    let centered_data = data.clone() - &mean;
-
-    // Calculate the covariance matrix of the centered data
-    let cov = centered_data.t().dot(&centered_data) / (data.len_of(Axis(0)) - 1) as f64;
-
-    // Calculate the eigenvalues and eigenvectors of the covariance matrix
-    let (eigenvalues, eigenvectors) = cov.eigh(UPLO::Lower).unwrap();
-
-    // Sort the eigenvalues and corresponding eigenvectors in descending order
-    let mut sorted_indices = eigenvalues
-        .indexed_iter()
-        .map(|(i, _)| i)
-        .collect::<Vec<_>>();
-    sorted_indices.sort_unstable_by(|&i, &j| eigenvalues[j].partial_cmp(&eigenvalues[i]).unwrap());
-
-    // let first_eigenvector = eigenvectors.select(Axis(0), &sorted_indices[0..1]);
-    // let projected_data = centered_data.dot(&first_eigenvector.t());
-
-    eigenvectors.select(Axis(0), &sorted_indices[0..1])
 }
