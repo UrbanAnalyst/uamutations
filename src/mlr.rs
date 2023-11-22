2ba5df3358ae9bb96039d3d517cf03812401b7b3
mpadge <mark.padgham@email.com>
2023-11-22 14:18:48 +0100

    add standardise-arrays fn for #11

--- src/mlr.rs
+++ src/mlr.rs
@@ -1,4 +1,4 @@
-use ndarray::{s, Array2};
+use ndarray::{s, Array2, Axis};
 use ndarray_linalg::LeastSquaresSvd;
 
 /// Calculates beta coefficients (slopes) of a multiple linear regression of dimensions [1.., _] of
@@ -57,6 +57,92 @@ pub fn mlr_beta(data: &Array2<f64>) -> Vec<f64> {
     b
 }
 
+pub fn standardise_arrays(
+    values1: &Array2<f64>,
+    values2: &Array2<f64>,
+) -> (Array2<f64>, Array2<f64>) {
+    let sum_values1: ndarray::Array1<f64> = values1.axis_iter(Axis(0)).map(|v| v.sum()).collect();
+    let sum_values2: ndarray::Array1<f64> = values2.axis_iter(Axis(0)).map(|v| v.sum()).collect();
+    let sum_values: ndarray::Array1<f64> = &sum_values1 + &sum_values2;
+
+    let sum_values1_sq: ndarray::Array1<f64> = values1
+        .axis_iter(Axis(0))
+        .map(|v| v.mapv(|x| x.powi(2)).sum())
+        .collect();
+    let sum_values2_sq: ndarray::Array1<f64> = values2
+        .axis_iter(Axis(0))
+        .map(|v| v.mapv(|x| x.powi(2)).sum())
+        .collect();
+    let sum_values_sq: ndarray::Array1<f64> = &sum_values1_sq + &sum_values2_sq;
+
+    // Calculate standard deviations:
+    let nobs = 2.0 * values1.ncols() as f64;
+    let mean_vals: ndarray::Array1<f64> = &sum_values / nobs;
+    let std_devs: ndarray::Array1<f64> =
+        ((&sum_values_sq / nobs) - (&sum_values / nobs).mapv(|x| x.powi(2))).mapv(f64::sqrt);
+
+    let mut values1 = values1.clone();
+    let mut values2 = values2.clone();
+
+    // Transform values, but not of first column of reference values:
+    for (i, (&mean, &std_dev)) in mean_vals.iter().zip(std_devs.iter()).enumerate() {
+        if i != 0 {
+            values1
+                .index_axis_mut(Axis(0), i)
+                .mapv_inplace(|x| (x - mean) / std_dev);
+            values2
+                .index_axis_mut(Axis(0), i)
+                .mapv_inplace(|x| (x - mean) / std_dev);
+        }
+    }
+
+    (values1, values2)
+}
+
+/// Adjusts the first row of `values1` based on the multi-linear regression coefficients of the
+/// remaining rows of `values1` against `values2`.
+///
+/// This effectivly removes the dependence of the first row of `values1` by on all other
+/// variables/rows, and replaces it with the dependence of `values2` on the same variables.
+/// Importantly, this adjustment also standardises the values to a different scale.
+///
+/// # Arguments
+///
+/// * `values1` - A 2D array where the first row is the variable to be adjusted and the remaining
+/// rows are the other variables.
+/// * `values2` - A 2D array with the same structure as `values1`, used to calculate the MLR
+/// coefficients for adjustment.
+///
+/// * Example
+/// let mut v1 = array![[1.0, 2.0, 3.0, 4.0, 5.0], [2.1, 3.2, 4.1, 5.2, 5.9]];
+/// let v1_orig = v1.clone();
+/// let v2 = array![[1.0, 2.0, 3.0, 4.0, 5.0], [3.1, 4.3, 5.3, 6.5, 7.3]];
+/// adj_for_beta(&mut v1, &v2);
+/// assert_ne!(v1, v1_orig, "v1 should differ from v1_orig");
+/// assert_eq!(
+///     v1.slice(s![1.., ..]),
+///     v1_orig.slice(s![1.., ..]),
+///     "Only the first row of v1 should be different"
+/// );
+pub fn adj_for_beta(values1: &mut Array2<f64>, values2: &Array2<f64>) {
+    // standardise inputs to same scales, excluding first row/variable:
+    let (values1_std, values2_std) = standardise_arrays(values1, values2);
+    // Calculate MLR regression coefficients between first variables and all others:
+    let beta1 = mlr_beta(&values1_std);
+    let beta2 = mlr_beta(&values2_std);
+    // Then adjust `values1` by removing its dependence on those variables, and replacing with the
+    // dependnece of values2 on same variables:
+    let mut result = ndarray::Array1::zeros(values1.ncols());
+    for i in 0..values1.ncols() {
+        let b1 = ndarray::Array1::from(beta1.clone());
+        let b2 = ndarray::Array1::from(beta2.clone());
+        let values_slice = values1.slice(s![1.., i]).to_owned();
+        let product = &values_slice * (1.0 + &b2 - &b1);
+        result[i] = product.sum();
+    }
+    values1.row_mut(0).assign(&result);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -78,5 +164,22 @@ mod tests {
         ];
         let result_3 = mlr_beta(&data_3);
         assert_eq!(result_3.len(), 2);
+    }
+
+    #[test]
+    fn test_adj_for_beta() {
+        let mut v1 = array![[1.0, 2.0, 3.0, 4.0, 5.0], [2.1, 3.2, 4.1, 5.2, 5.9]];
+        let v1_orig = v1.clone();
+        let v2 = array![[1.0, 2.0, 3.0, 4.0, 5.0], [3.1, 4.3, 5.3, 6.5, 7.3]];
+        adj_for_beta(&mut v1, &v2);
+        assert_ne!(
+            v1, v1_orig,
+            "v1 should be different from v1_orig after adj_for_beta"
+        );
+        assert_eq!(
+            v1.slice(s![1.., ..]),
+            v1_orig.slice(s![1.., ..]),
+            "Only the first row of v1 should be different"
+        );
     }
 }
