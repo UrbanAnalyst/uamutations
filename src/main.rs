f2c64e4362a6fcec56c5ccff8b7ec70f3af3da93
mpadge <mark.padgham@email.com>
2024-01-30 15:25:46 +0100

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,22 @@ extern crate uamutations;
 pub mod mlr;
 pub mod read_write_file;
 
-// const NENTRIES: usize = 10000;
-const NENTRIES: usize = 1000;
+const NENTRIES: usize = 10000;
+// const NENTRIES: usize = 1000;
 
-// const FNAME1: &str = "berlin.json";
-// const FNAME2: &str = "paris.json";
-const FNAME1: &str = "./test_resources/dat1.json";
-const FNAME2: &str = "./test_resources/dat2.json";
-const VARNAME: &str = "bike_index";
+const FNAME1: &str = "berlin.json";
+const FNAME2: &str = "paris.json";
+// const FNAME1: &str = "./test_resources/dat1.json";
+// const FNAME2: &str = "./test_resources/dat2.json";
+const VARNAME: &str = "times_rel";
 const OUTFILENAME: &str = "output.txt";
 
 /// Entry point for the Urban Analyst mutation algorithm.
 ///
 /// This exists only to locally call and run the library.
 fn main() {
-    let varextra = vec!["natural".to_string(), "social_index".to_string()];
-    // let varextra: Vec<String> = Vec::new();
+    // let varextra = vec!["natural".to_string(), "social_index".to_string()];
+    let varextra: Vec<String> = Vec::new();
 
     let varsall: Vec<String> = vec![VARNAME.to_string()];
     let varsall = [varsall, varextra].concat();
